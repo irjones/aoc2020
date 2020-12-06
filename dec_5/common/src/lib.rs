233022ed7b30cc8a28e2344f859014448856070e
Ian Jones <ian.reed.jones@gmail.com>
2020-12-05 21:40:15 -0800

    TODO for rust version

---
+++ dec_5/common/src/lib.rs
@@ -0,0 +1,11 @@
+pub mod day_five {
+    // TODO
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
---
+++ dec_6/js_sln/main.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+
+// TODO: rust version
+
+const data = fs.readFileSync("/Users/ianjones/Desktop/day6").toString();
+
+const grouped = data.split("\n\n");
+
+const pt1 = grouped
+  .map((g) => new Set(g.split("\n").join("").split("")))
+  .map((s) => Array.from(s).length)
+  .reduce((acc, next) => (acc = acc + next));
+
+const pt2 = grouped
+  .map((g) => g.split("\n").filter((s) => Boolean(s)))
+  .map((arr) => arr.map((s) => s.split("")))
+  .map(toIntersections)
+  .map((inter) => inter.length)
+  .reduce((acc, next) => (acc = acc + next));
+
+function toIntersections(arrArr) {
+  const sets = [];
+  for (let arr of arrArr) {
+    sets.push(new Set(arr));
+  }
+  return Array.from(
+    sets.reduce((acc, next) => acc.intersection(next)).values()
+  );
+}
+
+console.log(`Pt 1: ${pt1}\n Pt 2: ${pt2}`);
